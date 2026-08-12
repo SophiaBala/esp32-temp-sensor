cbf273c1d8241bf52d1debcb537e4b9f9f70a0c2
sofia bala <sofiabala06@gmail.com>
2026-08-12 16:46:56 +0300

    test led blink

--- main/main.c
+++ main/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <driver/gpio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#define LED_PIN 2
 
 void app_main(void)
 {
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    while(1){
+        gpio_set_level(LED_PIN, 0);
+        vTaskDelay(pdMS_TO_TICKS(2000));
 
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
