0e916d1bc4a5dee7f2cb2885f6cba587b728a73a
sofia bala <sofiabala06@gmail.com>
2026-08-13 00:04:11 +0300

    add 1-wire presence detection

--- main/main.c
+++ main/main.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
-#include <driver/gpio.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_rom_sys.h"
+#include "esp_log.h"
 
-#define LED_PIN 2
+#define ONEWIRE_GPIO 4
+static const char *TAG = "onewire";
+
+static bool on_presence(void){
+    gpio_set_direction(ONEWIRE_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_level(ONEWIRE_GPIO, 0);
+    esp_rom_delay_us(480);
+
+    gpio_set_level(ONEWIRE_GPIO, GPIO_MODE_INPUT);
+    esp_rom_delay_us(70);
+
+    bool presence = (gpio_get_level(ONEWIRE_GPIO) == 0);
+    esp_rom_delay_us(410);
+
+    return presence;
+}
 
 void app_main(void)
 {
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
-    while(1){
-        gpio_set_level(LED_PIN, 0);
-        vTaskDelay(pdMS_TO_TICKS(2000));
+    gpio_reset_pin(ONEWIRE_GPIO);
+    gpio_set_pull_mode(ONEWIRE_GPIO, GPIO_PULLUP_ONLY);
 
-        gpio_set_level(LED_PIN, 1);
+    while (1) {
+        bool found = on_presence();
+        ESP_LOGI(TAG, "Presence: %s", found ? "YES" : "NO");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
