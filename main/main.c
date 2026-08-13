c855b59cd41c1a85b1008f83940543584ded96ef
sofia bala <sofiabala06@gmail.com>
2026-08-13 23:10:16 +0300

    complete ds18b20 temperature reading

--- main/main.c
+++ main/main.c
@@ -13,7 +13,7 @@ static bool on_presence(void){
     gpio_set_level(ONEWIRE_GPIO, 0);
     esp_rom_delay_us(480);
 
-    gpio_set_level(ONEWIRE_GPIO, GPIO_MODE_INPUT);
+    gpio_set_direction(ONEWIRE_GPIO, GPIO_MODE_INPUT);
     esp_rom_delay_us(70);
 
     bool presence = (gpio_get_level(ONEWIRE_GPIO) == 0);
@@ -22,14 +22,85 @@ static bool on_presence(void){
     return presence;
 }
 
+static void write_bit(int bit){
+    gpio_set_direction(ONEWIRE_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_level(ONEWIRE_GPIO, 0);
+    esp_rom_delay_us(bit ? 6 : 60);
+    gpio_set_direction(ONEWIRE_GPIO, GPIO_MODE_INPUT);
+    esp_rom_delay_us(bit ? 64 : 10);
+}
+
+
+static int read_bit(void){
+    gpio_set_direction(ONEWIRE_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_level(ONEWIRE_GPIO, 0);
+    esp_rom_delay_us(2);
+    gpio_set_direction(ONEWIRE_GPIO, GPIO_MODE_INPUT);
+    esp_rom_delay_us(10);
+
+    int bit = gpio_get_level(ONEWIRE_GPIO);
+    esp_rom_delay_us(50);
+    return bit;
+}
+
+static void write_byte(uint8_t byte){
+    for (int i=0; i < 8; i++){
+        write_bit(byte&1);
+        byte>>=1;
+    }
+}
+
+static uint8_t read_byte(void){
+    uint8_t byte = 0;
+    for (int i=0; i <= 8; i++){
+        byte |= (read_bit()<<i);
+    }
+    return byte;
+}
+
+static bool read_ds18b20(float *final_temperature){
+    uint8_t data[9];
+
+    if(on_presence() == false){
+        return false;
+    }
+
+    write_byte(0xCC);
+    write_byte(0x44);
+
+    vTaskDelay(pdMS_TO_TICKS(700));
+
+    if(on_presence() == false){
+        return false;
+    }
+
+    write_byte(0xCC);
+    write_byte(0xBE);
+
+    for(int i = 0; i<9; i++){
+        data[i] = read_byte();
+    }
+
+    int16_t temperature;
+    temperature = (data[1] << 8) | data[0];
+
+    *final_temperature = temperature / 16.0f;
+
+    return true;
+}
+
 void app_main(void)
 {
     gpio_reset_pin(ONEWIRE_GPIO);
     gpio_set_pull_mode(ONEWIRE_GPIO, GPIO_PULLUP_ONLY);
 
     while (1) {
-        bool found = on_presence();
-        ESP_LOGI(TAG, "Presence: %s", found ? "YES" : "NO");
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        float t;
+        if (read_ds18b20(&t)) {
+            ESP_LOGI(TAG, "Temperature: %.2f C", t);
+        } else {
+            ESP_LOGW(TAG, "failed");
+        }
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
